9ff567a7cdf440bde58df342c6e900d08d60db15
luca.pandini <luca.pandini@glovoapp.com>
2024-01-24 10:16:22 +0100

    add mockk dependency

--- kotlin/junit/build.gradle.kts
+++ kotlin/junit/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.22")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("io.mockk:mockk:1.13.9")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
